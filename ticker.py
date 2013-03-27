2e8dd0e26b99620a94ce6e8b78805ce530be362c
HackFisher <wxiaoyin@gmail.com>
2013-03-28 00:12:41 +0800

    ticker channel

--- ticker.py
+++ ticker.py
@@ -7,6 +7,7 @@ jinja_environment = jinja2.Environment(
 
 import cgi
 import webapp2
+import json
 
 from google.appengine.api import users
 from google.appengine.api import channel
@@ -22,7 +23,7 @@ class MainPage(webapp2.RequestHandler):
             return
 		
         client_id = user.user_id() + str(int(round(time.time() * 1000)))
-        token = channel.create_channel(user.user_id())
+        token = channel.create_channel(client_id)
         clients.add(client_id)
 
         template_values = {'token': token,
@@ -40,19 +41,18 @@ class SubmitPage(webapp2.RequestHandler):
         template = jinja_environment.get_template('submit.html')
         self.response.out.write(template.render(template_values))
 
+class SendTicker(webapp2.RequestHandler):
     def post(self):
-        amount_in = self.request.get("amount_in")
-        amount_out = self.request.get("amount_out")
-        current = self.request.get("current")
-        high = self.request.get("high")
-        low = self.request.get("low")
+        ticker = {}
+        ticker['amout_in'] = self.request.get("amount_in")
+        ticker['amount_out'] = self.request.get("amount_out")
+        ticker['current'] = self.request.get("current")
+        ticker['high'] = self.request.get("high")
+        ticker['low'] = self.request.get("low")
+        ticker_str = json.dumps(ticker)
 
-class TickerPage(webapp2.RequestHandler):
-    def post(self):
-        self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('content')))
-        self.response.out.write('</pre></body></html>')
+        for client_id in clients:
+            channel.send_message(client_id, ticker_str)
 		
-		
-app = webapp2.WSGIApplication([('/', MainPage), ('/ticker', TickerPage), ('/submit', SubmitPage)],
+app = webapp2.WSGIApplication([('/', MainPage), ('/send', SendTicker), ('/submit', SubmitPage)],
                               debug=True)
